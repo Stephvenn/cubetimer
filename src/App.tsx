f32453b568d108f465b979805f8d712116b5b175
Stephvenn <stephenmkshin@gmail.com>
2023-04-19 15:15:44 -0400

    asdfasdf

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Timer from './components/Timer';
 import ScrambleBar from './components/ScrambleBar';
-import Stopwatch from './components/Stopwatch';
 
 function App() {
   return (
