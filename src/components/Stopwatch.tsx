dc94c5513e14c54c79ca29ec36a8da5f27a4bc1f
Stephvenn <stephenshin792@hotmail.ca>
2023-04-19 14:09:53 -0400

    jhb

--- src/components/Stopwatch.tsx
+++ src/components/Stopwatch.tsx
@@ -1,53 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function Stopwatch(): JSX.Element {
-  const [startTime, setStartTime] = useState<number | null>(null);
-  const [elapsedTime, setElapsedTime] = useState<number>(0);
-  const [isRunning, setIsRunning] = useState<boolean>(false);
+	const [startTime, setStartTime] = useState<number | null>(null);
+	const [elapsedTime, setElapsedTime] = useState<number>(0);
+	const [isRunning, setIsRunning] = useState<boolean>(false);
 
-  useEffect(() => {
-    function handleKeyPress(event: KeyboardEvent): void {
-      if (event.code === 'Space') {
-        setIsRunning(prevState => !prevState);
-      }
-    }
+	useEffect(() => {
+		function handleKeyPress(event: KeyboardEvent): void {
+			if (event.code === "Space") {
+				setIsRunning((prevState) => !prevState);
+			}
+		}
 
-    window.addEventListener('keydown', handleKeyPress);
+		window.addEventListener("keydown", handleKeyPress);
 
-    return () => {
-      window.removeEventListener('keydown', handleKeyPress);
-    };
-  }, []);
+		return () => {
+			window.removeEventListener("keydown", handleKeyPress);
+		};
+	}, []);
 
-  useEffect(() => {
-    let intervalId: NodeJS.Timeout | undefined;
+	useEffect(() => {
+		let intervalId: NodeJS.Timeout | undefined;
 
-    if (isRunning) {
-      setStartTime(Date.now() - elapsedTime);
+		if (isRunning) {
+			setStartTime(Date.now() - elapsedTime);
 
-      intervalId = setInterval(() => {
-        setElapsedTime(Date.now() - (startTime || 0));
-      }, 10);
-    } else {
-      clearInterval(intervalId);
-    }
+			intervalId = setInterval(() => {
+				setElapsedTime(Date.now() - (startTime || 0));
+			}, 10);
+		} else {
+			clearInterval(intervalId);
+		}
 
-    return () => clearInterval(intervalId);
-  }, [isRunning, startTime, elapsedTime]);
+		return () => clearInterval(intervalId);
+	}, [isRunning, startTime, elapsedTime]);
 
-  function formatTime(time: number): string {
-    const minutes = Math.floor(time / 60000);
-    const seconds = Math.floor((time / 1000) % 60);
-    const milliseconds = Math.floor((time % 1000) / 10);
-    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}.${milliseconds.toString().padStart(2, '0')}`;
-  }
+	function formatTime(time: number): string {
+		const minutes = Math.floor(time / 60000);
+		const seconds = Math.floor((time / 1000) % 60);
+		const milliseconds = Math.floor((time % 1000) / 10);
+		return `${minutes.toString().padStart(2, "0")}:${seconds
+			.toString()
+			.padStart(2, "0")}.${milliseconds.toString().padStart(2, "0")}`;
+	}
 
-  return (
-    <div>
-      <h1>{formatTime(elapsedTime)}</h1>
-      <p>Press spacebar to start/stop the stopwatch</p>
-    </div>
-  );
+	return (
+		<div>
+			<h1>{formatTime(elapsedTime)}</h1>
+			<p>Press spacebar to start/stop the stopwatch</p>
+		</div>
+	);
 }
 
 export default Stopwatch;
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,55 +1,53 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, { useState, useRef, useEffect } from "react";
 
 export default function Timer() {
+	const [time, setTime] = useState(0);
+	const [isRunning, setIsRunning] = useState(false);
+	const startTimeRef: any = useRef();
+	const requestRef: any = useRef();
 
-    const [time, setTime] = useState(0);
-    const [isRunning, setIsRunning] = useState(false);
-    const startTimeRef: any = useRef();
-    const requestRef: any = useRef();
+	const updateElapsedTime = (timestamp: any) => {
+		if (!isRunning) {
+			return;
+		}
+		const currentTime = new Date().getTime();
+		const elapsed = currentTime - startTimeRef.current;
+		setTime(elapsed);
+		requestRef.current = requestAnimationFrame(updateElapsedTime);
+	};
 
-    const updateElapsedTime = (timestamp: any) => {
-      if (!isRunning) {
-        return;
-      }
-      const currentTime = new Date().getTime();
-      const elapsed = currentTime - startTimeRef.current;
-      setTime(elapsed);
-      requestRef.current = requestAnimationFrame(updateElapsedTime);
-    };
+	const formatElapsedTime = (elapsedTime: number) => {
+		const minutes = Math.floor(elapsedTime / 60000);
+		const seconds = Math.floor((elapsedTime % 60000) / 1000);
+		const milliseconds = elapsedTime % 1000;
+		return `${minutes.toString().padStart(2, "0")}:${seconds
+			.toString()
+			.padStart(2, "0")}:${milliseconds.toString().padStart(3, "0")}`;
+	};
 
-    const formatElapsedTime = (elapsedTime: number) => {
-      const minutes = Math.floor(elapsedTime / 60000);
-      const seconds = Math.floor((elapsedTime % 60000) / 1000);
-      const milliseconds = elapsedTime % 1000;
-      return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${milliseconds.toString().padStart(3, '0')}`;
-    };
+	useEffect(() => {
+		function handleKeyPress(event: KeyboardEvent): void {
+			if (event.code === "Space") {
+				console.log("Spacebar pressed");
+				setIsRunning((prev) => !prev);
+			}
+		}
+		window.addEventListener("keydown", handleKeyPress);
 
-    useEffect(() => {
-      function handleKeyPress(event: KeyboardEvent): void {
-        if (event.code === 'Space') {
-          console.log('Spacebar pressed');
-          setIsRunning(prev => !prev);
-        }
-      }
-      window.addEventListener('keydown', handleKeyPress);
-      
-      // clean up
-      return () => {
-        window.removeEventListener('keydown', handleKeyPress);
-      };
-    }, []);
+		// clean up
+		return () => {
+			window.removeEventListener("keydown", handleKeyPress);
+		};
+	}, []);
 
-    useEffect(() => {
-      if (isRunning){
-        startTimeRef.current = new Date().getTime();
-        requestRef.current = requestAnimationFrame(updateElapsedTime);
-      } else{
-        cancelAnimationFrame(requestRef.current);
-      }
-    }, [isRunning]);
+	useEffect(() => {
+		if (isRunning) {
+			startTimeRef.current = new Date().getTime();
+			requestRef.current = requestAnimationFrame(updateElapsedTime);
+		} else {
+			cancelAnimationFrame(requestRef.current);
+		}
+	}, [isRunning]);
 
-
-    return (
-        <p className='timer'>{formatElapsedTime(time)}</p>
-    );
+	return <p className="timer">{formatElapsedTime(time)}</p>;
 }
